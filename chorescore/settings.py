3dcf39d9cb21196154ecfa8a5c7aabbdc873d3d2
Adam Brenecki <adam@brenecki.id.au>
2013-07-10 12:03:34 +0930

    Fix syntax

--- chorescore/settings.py
+++ chorescore/settings.py
@@ -63,7 +63,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ('chorescore', os.path.join(REPO_ROOT, 'STATIC'),
+    ('chorescore', os.path.join(REPO_ROOT, 'STATIC')),
 )
 
 # List of finder classes that know how to find static files in
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'chores',
 )
 
 # A sample logging configuration. The only tangible logging
