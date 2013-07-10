8637bff256aa3403aa82755637cab968ad97784d
Adam Brenecki <adam@brenecki.id.au>
2013-07-10 12:04:50 +0930

    Add admin

--- chorescore/settings.py
+++ chorescore/settings.py
@@ -118,9 +118,9 @@ INSTALLED_APPS = (
     'south',
     
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
     'chores',
 )
 
